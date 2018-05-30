72d02cfb88c902a141b9c45825c875e50b2c6787
niceman <xu.zhidong@gcexe.com>
2018-05-30 09:47:56 +0800

    update

--- src/main/java/com/fineframework/HelloController.java
+++ src/main/java/com/fineframework/HelloController.java
@@ -1,12 +1,14 @@
 package com.fineframework;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
 	@RequestMapping("/")
+	@ResponseBody
 	public String hello() {
 		return "hello world";
 	}
